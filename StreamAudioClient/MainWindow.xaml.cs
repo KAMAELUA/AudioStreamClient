8612ee9e1078327e92d07058ceb68686b019909e
Andrey Chugay <kamaelua@gmail.com>
2016-05-24 12:33:57 +0300

    some updates

--- StreamAudioClient/MainWindow.xaml.cs
+++ StreamAudioClient/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using NAudio.Wave;
+﻿using NAudio.Dsp;
+using NAudio.Wave;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -27,14 +28,13 @@ namespace StreamAudioClient
     public partial class MainWindow : Window
     {
         public int port = 11001; //умолчание
-        public string adres = "127.0.0.1"; //умолчание
+        //public string adres = "192.168.0.102"; //умолчание
         WaveOut waveOut;
 
         WaveFormat waveFormat;
 
-        IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, 11001);
+        //IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, 11001);
 
-        //Socket udpsock = new Socket(SocketType.Dgram, ProtocolType.Udp);
         public MainWindow()
         {
             InitializeComponent();
@@ -42,73 +42,32 @@ namespace StreamAudioClient
             waveOut = new WaveOut();
             waveFormat = WaveFormat.CreateIeeeFloatWaveFormat(44100, 2);
             //waveFormat = WaveFormat.CreateIeeeFloatWaveFormat()
-
-
-
-            //NetworkStream ns = listener.GetStream();
-
-
+            
         }
 
         public void ConnectToServer()
         {
-            //TcpListener l = new TcpListener(IPAddress.Parse(adres), port); //и на нем у нас висит сервер
-            
-            TcpClient client = new TcpClient(adres, port); //IP адрес сервера и порт на котором он висит
+            TcpClient client = new TcpClient(FieldIpAddress.Text, port); //IP адрес сервера и порт на котором он висит
             NetworkStream NWS = client.GetStream();
             BinaryReader R = new BinaryReader(NWS); //поток для принятия данных
 
             var rawSource = new RawSourceWaveStream(NWS, waveFormat);
-
-            waveOut.Init(rawSource);
-            waveOut.Play();
-            //BinaryWriter W = new BinaryWriter(NWS); //поток для отправки данных
-
-
-        }
-
-        /*public void InitilizeElements()
-        {
+            //ISampleProvider s = new ISampleProvider();
             
-        }*/
+            waveOut.Init(rawSource);
+            
+            waveOut.Play();
+            SampleAgg
+        }
 
         private void BtnStart_Click(object sender, RoutedEventArgs e)
         {
             ConnectToServer();
         }
 
-        /*private void StartListener()
+        private void FieldIpAddress_TextChanged(object sender, TextChangedEventArgs e)
         {
-            bool done = false;
 
-            
-
-            try
-            {
-                Console.WriteLine("Waiting for broadcast");
-                while (!done)
-                {
-                    Console.WriteLine("Waiting for broadcast");
-                    //byte[] bytes = listener.
-                    //stream.WriteAsync(bytes, 0, bytes.Length);
-                    
-                    
-                    //waveOut.Play();
-
-                    //Console.WriteLine("Received broadcast from {0} :\n {1}\n", groupEP.ToString(), bytes.Length);
-                }
-
-
-
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
-            finally
-            {
-                listener.Close();
-            }
-        }*/
+        }
     }
 }
